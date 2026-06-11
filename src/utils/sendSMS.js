47ee8f4a4a62d7e3168a865ee9ce4485a583f6f5
Prince Mohobia <mohobiaprince31@gmail.com>
2026-06-11 17:39:41 +0530

    Add MSG91 logging

--- src/utils/sendSMS.js
+++ src/utils/sendSMS.js
@@ -1,5 +1,4 @@
-const axios =
-  require("axios");
+const axios = require("axios");
 
 const sendSMS = async (
   mobile,
@@ -7,36 +6,40 @@ const sendSMS = async (
 ) => {
   try {
 
-    await axios.post(
-      "https://control.msg91.com/api/v5/otp",
-      {
-        mobile:
-          `91${mobile}`,
-        otp,
-      },
-      {
-        headers: {
-          authkey:
-            process.env
-              .MSG91_API_KEY,
-          "Content-Type":
-            "application/json",
+    const response =
+      await axios.post(
+        "https://control.msg91.com/api/v5/otp",
+        {
+          mobile: `91${mobile}`,
+          otp: otp,
+          template_id:
+            process.env.MSG91_TEMPLATE_ID,
         },
-      }
+        {
+          headers: {
+            authkey:
+              process.env.MSG91_API_KEY,
+            "Content-Type":
+              "application/json",
+          },
+        }
+      );
+
+    console.log(
+      "MSG91 Response:",
+      response.data
     );
 
   } catch (error) {
 
     console.error(
+      "MSG91 Error:",
       error.response?.data ||
       error.message
     );
 
-    throw new Error(
-      "Failed to send OTP"
-    );
+    throw error;
   }
 };
 
-module.exports =
-  sendSMS;
+module.exports = sendSMS;
