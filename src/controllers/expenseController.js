a1bf6f8662350714dd6ded6f9e7613e56c11bbdf
Prince Mohobia <mohobiaprince31@gmail.com>
2026-06-11 10:58:35 +0530

    Fix SMTP config for Render

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -857,17 +857,26 @@ const sendExpenseReport = async (
     );
 
     const transporter =
-      nodemailer.createTransport({
-        service: "gmail",
 
-        auth: {
-          user:
-            process.env.EMAIL_USER,
+  nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true only for 465
 
-          pass:
-            process.env.EMAIL_PASS,
-        },
-      });
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  await transporter.verify();
+
+console.log(
+  "SMTP Connection Successful"
+);
 
     await transporter.sendMail({
       from:
